1293d2f7544d5ddcff1a674c253879de60706be1
william341 <wwokwilliam@gmail.com>
2017-11-12 15:46:41 -0700

    Add files via upload

--- role.soldier.js
+++ role.soldier.js
@@ -4,30 +4,21 @@ var roleSoldier = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-
-        if(creep.carry.energy == 0) {
-            creep.say('harvest');
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
-        if(creep.carry.energy == creep.carryCapacity) {
-            creep.say('attack');
-        }
+        checkenergy('fight', creep);
         var enemies = creep.room.find(Game.HOSTILE_CREEPS);
-        if(!(enemies[0] == null)){
-            creep.moveTo(enemies[0],{visualizePathStyle: {stroke: '#ff0000'}});
-            creep.attack(enemies[0]);
-        }
-
-    /*
-        else {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ff0000'}});
+        if(creep.memory.acting) {
+            if(enemies.length){
+                if(creep.attack(enemies[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(enemies[0],{visualizePathStyle: {stroke: '#ff0000'}});
+                }
             }
-        }*/
+            else { 
+                roleHarvester.run(creep);
+            }
+        }
+        else {
+            harvest(creep);
+        }
     }
 };
 
