4815792da66cfcec61619db244c4c4d5985ac8b9
william341 <wwokwilliam@gmail.com>
2017-11-12 17:42:43 -0700

    new thing

--- backend.lazycodes.js
+++ backend.lazycodes.js
@@ -6,15 +6,10 @@
  * var mod = require('backend.lazycodes');
  * mod.thing == 'a thing'; // true
  */
+require('backend.config');
 
 global["defbodycreate"] = function(spawn, nametouse, role) {
-    if(role == 'soldier'){
-        Game.spawns[spawn].spawnCreep( [WORK, CARRY, MOVE, ATTACK, ATTACK], nametouse,{ memory: { role: role } } );
-    }else if(role == 'harvester'){
-        Game.spawns[spawn].spawnCreep( [WORK, CARRY, MOVE], nametouse,{ memory: { role: role } } );
-    }else{
-        Game.spawns[spawn].spawnCreep( [WORK, CARRY, MOVE], nametouse,{ memory: { role: role } } );
-    }
+  Game.spawns[spawn].spawnCreep( bodies[role], nametouse,{ memory: { role: role } } );
 }
 
 global["checkifexists"] = function(role, amount, spawn) {
@@ -59,7 +54,51 @@ global["randomint"] = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+global["method1_spawn"] = function() {
+  var totals = {
+      "builder":0,
+      "harvester":0,
+      "upgrader":0,
+      "repairman":0,
+      "soldier":0
+  };
 
+  for(var name in Game.creeps) {
+      var creep = Game.creeps[name];
+      totals[creep.memory.role]++;
+  }
+
+  var lowest = "harvester";
+  var low = method1_max;
+
+  for(var role in totals){
+      if(totals[role]<low && typesenabled[role]){
+          low = totals[role];
+          lowest = role;
+      }
+
+  }
+
+  checkifexists(lowest, method1_max, 'Spawn1');
+
+  if (method1_statusmessages = true) {
+    Game.spawns['Spawn1'].room.visual.text(
+        "we need "+lowest+"s we only have "+low.toString(),
+        Game.spawns['Spawn1'].pos.x + 1,
+        Game.spawns['Spawn1'].pos.y,
+        {align: 'left', opacity: 0.8});
+  }
+}
+
+global["method0_spawn"] = function() {
+  for(var role in typesenabled) {
+    checkifexists(role, method0_amounts[role], "Spawn1");
+  }
+
+  if (method0_statusmessages = true) {
+    checkspawnstatus("Spawn1");
+  }
+}
 
 module.exports = {
 
--- main.js
+++ main.js
@@ -4,60 +4,27 @@ var roleBuilder = require('role.builder');
 var roleRepairman = require('role.repairman');
 var roleSoldier = require('role.soldier');
 require('backend.lazycodes');
+require('backend.config');
 module.exports.loop = function () {
 
-    var totals = {
-        "builder":0,
-        "harvester":0,
-        "upgrader":0,
-        "repairman":0,
-        "soldier":0,
-        
-    };
+  for(var name in Memory.creeps) {
+      if(!Game.creeps[name]) {
+          delete Memory.creeps[name];
+          console.log('Clearing non-existing creep memory:', name);
+      }
 
-    for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
-        }   
-        
-    }
-    
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        totals[creep.memory.role]++;
-    }
-    
-    var lowest = "harvester";
-    var low = 10000;
-    
-    for(var role in totals){
-        if(totals[role]<low){
-            low = totals[role];
-            lowest = role;
-        }
-        
-    }
-    /*
-    checkifexists('builder', 3, 'Spawn1');
-    checkifexists('harvester', 5, 'Spawn1');
-    checkifexists('upgrader', 5, 'Spawn1');
-    checkifexists('repairman', 3, 'Spawn1');
-    checkifexists('soldier', 1, 'Spawn1')
-    */
-    
-    checkifexists(lowest, 10000, 'Spawn1');
-    
-    Game.spawns['Spawn1'].room.visual.text(
-        "we need "+lowest+"s we only have "+low.toString(),
-        Game.spawns['Spawn1'].pos.x + 1,
-        Game.spawns['Spawn1'].pos.y,
-        {align: 'left', opacity: 0.8});
+  }
 
+  if(spawnmethod == 0) {
+    method0_spawn();
+  }
+  else if(spawnmethod == 1) {
+    method1_spawn();
+  }
+  else {
+    method0_spawn();
+  }
 
-    
-    //checkspawnstatus("Spawn1");
-    
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
