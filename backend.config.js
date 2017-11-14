aafa23d9405380eb347f46d2b6b224475b6a96bc
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-11-13 21:49:31 -0500

    tweaks

--- backend.config.js
+++ backend.config.js
@@ -1,5 +1,5 @@
 //Global Settings
-global["spawnmethod"] = 0; //0 for william341's method 1 for Derpy's method
+global["spawnmethod"] = 1; //0 for william341's method 1 for Derpy's method
 global["typesenabled"] =
   {
     "builder":true,
@@ -7,19 +7,18 @@ global["typesenabled"] =
     "upgrader":true,
     "repairman":true,
     "soldier":true,
-    "healer":true
+    "healer":false
   };
 global["bodies"] =
   {
-    "builder":[WORK, CARRY, MOVE],
-    "harvester":[WORK, CARRY, MOVE],
-    "upgrader":[WORK, CARRY, MOVE],
-    "repairman":[WORK, CARRY, MOVE],
+    "builder":[WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+    "harvester":[WORK, WORK, CARRY, MOVE, MOVE, MOVE],
+    "upgrader":[WORK, WORK, WORK, CARRY, CARRY, MOVE],
+    "repairman":[WORK, WORK, CARRY, MOVE, MOVE, MOVE],
     "soldier":[WORK, CARRY, MOVE, ATTACK, ATTACK],
     "healer":[WORK, CARRY, MOVE]
   };
 global["autoroad"] = true;
-global["autosafemode"] = true;
 
 //william341 method settings
 global["method0_amounts"] =
@@ -36,3 +35,4 @@ global["method0_statusmessages"] = true;
 //Derpy method settings
 global["method1_max"] = 10000
 global["method1_statusmessages"] = true;
+global["autosafemode"] = false;
